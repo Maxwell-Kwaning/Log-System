b7b49aa38931f12506915d8f29ffce3c45790014
Maxwell-Kwaning <maxkwaning100@gmail.com>
2022-05-25 09:28:28 +0000

    Admin login and user registration page

---
+++ components/ProfilePictureUploader.js
@@ -0,0 +1,44 @@
+import { Upload } from "antd";
+import ImgCrop from "antd-img-crop";
+import { useState } from "react";
+
+const ProfilePictureUploader = () => {
+  const [fileList, setFileList] = useState([]);
+
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const onPreview = async (file) => {
+    let src = file.url;
+
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow?.document.write(image.outerHTML);
+  };
+
+  return (
+    <ImgCrop rotate>
+      <Upload
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 1 && "+ Upload"}
+      </Upload>
+    </ImgCrop>
+  );
+};
+
+export default ProfilePictureUploader;
